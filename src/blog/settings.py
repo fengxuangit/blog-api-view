b72be3c42ac03c9766e05abe305461913e9cc3c4
fengxuangit <978348306@qq.com>
2017-01-21 11:39:31 +0800

    modify settings

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -145,3 +145,16 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
 
 
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+    ),
+    
+
+
+}
